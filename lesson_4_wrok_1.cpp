d3dd031c30f21f1d26dfa46806c6d524c462aada
Hornb00k <99634894+Hornb00k@users.noreply.github.com>
2022-03-09 05:25:59 +0800

    Create lesson_4_wrok_1.cpp

---
+++ lesson_4_wrok_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    string z[10]={
+        "ноль", "один","два","три","четыре",
+        "пять","шесть","семь","восемь","девять"
+    };
+    cout<<"Введите число";
+    cin>>a;
+    cout<<z[a];
+    return 0;
+}
