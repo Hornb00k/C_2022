10c8636b12fc9501b3b193847c5959d38a901001
Hornb00k <99634894+Hornb00k@users.noreply.github.com>
2022-03-10 00:42:26 +0800

    Create lesson_5_work_1.cpp

---
+++ lesson_5_work_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,result;
+    cout<<"Введите первое дробное число\n";
+    cin>>a;
+    cout<<"Введите второе дробное число\n";
+    cin>>b;
+    result=(a+b)*(a+b);
+    cout<<"Результат - "<<result<<"\n";
+}
