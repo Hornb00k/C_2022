754aaaa61056ab65b28dfc43f7a6b498dda6fb18
Hornb00k <99634894+Hornb00k@users.noreply.github.com>
2022-03-10 00:23:54 +0800

    Update lesson_4_work_2.cpp

--- lesson_4_work_2.cpp
+++ lesson_4_work_2.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <cstring>
-#include <cctype>
 
 using namespace std;
 
 int main()
 {
-    int a, b, i;
-    char x[];
-    int *c;
+    int a, i=0;
+    char x[80];
     cout << "Ввод символов" << "\n";
     cin >> x;
     a = strlen(x);
-    cout<< a << "\n";
-    for (i=0; i>a; i++){
-        *c = &x[i];
-        (*c)++;
+    while (1){
+    x[i]=x[i]+1;
+    i++;
+    if(a==i) break;
     }
     cout<<"Результат - "<<x;
     return 0;
